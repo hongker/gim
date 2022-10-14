8d3a1cfbebe3246c48fd8be4bfccf8ef4bf8335c
hongker <xiaok2013@live.com>
2022-10-14 23:16:23 +0800

    实现schema

--- framework/schema.go
+++ framework/schema.go
@@ -4,3 +4,17 @@ type Schema struct {
 	Protocol string
 	Addr     string
 }
+
+const (
+	tcp       = "tcp"
+	websocket = "websocket"
+	http      = "http"
+)
+
+func NewTcpSchema(address string) *Schema {
+	return &Schema{Protocol: tcp, Addr: address}
+}
+
+func NewWebsocketSchema(address string) *Schema {
+	return &Schema{Protocol: websocket, Addr: address}
+}
